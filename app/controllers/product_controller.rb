5756948343d35ec4afea965d77235bf0a0bf5089
Travis Castillo <tcastillo@crowdcompass.com>
2015-03-25 09:01:20 -0700

    stubbing in initial content

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,9 +1,13 @@
 class ProductController < ApplicationController
     def index
-
+        @products = products.list
     end
 
+    def show
+        @product = products.findById(id)
+
     def new
+
     end
 
     
