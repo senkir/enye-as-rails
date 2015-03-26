a27398a77ebab9ba82b86a96510117d7bd2e1b74
Travis Castillo <tcastillo@crowdcompass.com>
2015-03-25 20:40:52 -0700

    using description as schema definition

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150315155131) do
     t.string   "name"
     t.integer  "platform_id"
     t.string   "version"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
